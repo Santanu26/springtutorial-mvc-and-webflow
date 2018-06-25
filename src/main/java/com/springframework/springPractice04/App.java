068dd294584dc9e898b908969785e75417f6d9ea
Santanu <cushantanu@gmail.com>
2018-06-25 15:01:58 +0600

    Spring Project: Simple Bean Initialization using XML with MAVEN build Tool

---
+++ src/main/java/com/springframework/springPractice04/App.java
@@ -0,0 +1,15 @@
+package com.springframework.springPractice04;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+
+		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		Patient patient = (Patient) context.getBean("patient");
+		patient.speak();
+
+		((FileSystemXmlApplicationContext) context).close();
+	}
+}
---
+++ src/main/java/com/springframework/springPractice04/Patient.java
@@ -0,0 +1,8 @@
+package com.springframework.springPractice04;
+
+public class Patient {
+	public void speak() {
+		System.out.println("Help me...");
+	}
+
+}
