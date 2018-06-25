f6ae3c98957fd8e061b57a52082737b575406340
Santanu <cushantanu@gmail.com>
2018-06-25 15:06:03 +0600

    Loading Beans from Class Path Using ClassPathXMLApplicationContext

--- src/main/java/com/springframework/springPractice04/App.java
+++ src/main/java/com/springframework/springPractice04/App.java
@@ -1,15 +1,16 @@
 package com.springframework.springPractice04;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
 
-		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		ApplicationContext context = 
+				new ClassPathXmlApplicationContext("com/springframework/springPractice04/beans/beans.xml");
 		Patient patient = (Patient) context.getBean("patient");
 		patient.speak();
 
-		((FileSystemXmlApplicationContext) context).close();
+		((ClassPathXmlApplicationContext) context).close();
 	}
 }
