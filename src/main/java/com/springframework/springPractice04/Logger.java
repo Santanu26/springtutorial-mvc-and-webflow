f043768dd0e9cac0122aa24a39686085a6290570
Santanu <cushantanu@gmail.com>
2018-06-27 12:59:07 +0600

    Spring Annotation By Setter Injection

--- src/main/java/com/springframework/springPractice04/Logger.java
+++ src/main/java/com/springframework/springPractice04/Logger.java
@@ -2,16 +2,29 @@ package com.springframework.springPractice04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Logger {
-	@Autowired
+
 	private LogWriter consoleWriter;
-	@Autowired
+
 	private LogWriter fileWriter;
+
+	public LogWriter getConsoleWriter() {
+		return consoleWriter;
+	}
+
+	@Autowired
+	public void setConsoleWriter(LogWriter consoleWriter) {
+		this.consoleWriter = consoleWriter;
+	}
+
+	public LogWriter getFileWriter() {
+		return fileWriter;
+	}
+
+	@Autowired
+	public void setFileWriter(LogWriter fileWriter) {
+		this.fileWriter = fileWriter;
+	}
 
 	public void writeConsole(String text) {
 		consoleWriter.write(text);
